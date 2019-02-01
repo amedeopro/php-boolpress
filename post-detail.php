a9f084c1d0eb387b4712cf579d212c5a9250cd40
amedeopro <amedeopro@me.com>
2019-02-01 22:15:16 +0100

    prima parte funzionante, manca excerpt in pagina posts.php

--- post-detail.php
+++ post-detail.php
@@ -4,10 +4,41 @@
 
   include 'data.php';
 
-  foreach ($posts as $post) {
-    if ($post['slug'] == $slug) {
-      echo $post['title'];
-    }
-  }
+   ?>
 
- ?>
+   <!DOCTYPE html>
+   <html lang="it" dir="ltr">
+     <head>
+       <link rel="stylesheet" href="css/app.css">
+       <meta charset="utf-8">
+       <title>BlogPress - <?php echo $slug; ?></title>
+     </head>
+     <body>
+
+       <?php foreach ($posts as $post) { ?>
+         <?php if ($post['slug'] == $slug) { ?>
+           <div class="container">
+
+             <div class="post">
+               <div class="titolo">
+
+                   <h1><?php echo $post['title']; ?></h1>
+
+               </div>
+               <div class="giorno-pubblicazione">
+                 <p><?php echo date($post['published_at']); ?></p>
+               </div>
+               <div class="estratto">
+                 <?php echo trim($post['content']); ?>
+               </div>
+             </div>
+
+
+           </div>
+         <?php } ?>
+       <?php } ?>
+
+
+
+     </body>
+   </html>
